fbd2a1d2e53198c3f4f692127bd12f3512ace6d9
Álvaro Menéndez Ros <alvaro.mrgr@gmail.com>
2025-09-05 18:45:25 +0200

    Extracted data

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,29 @@
-def
+import zipfile
+import os
+
+
+def extract_zip(zip_path, extract_to=None):
+    os.makedirs(extract_to, exist_ok=True)
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+        print(f"Extracted all files to: {extract_to}")
+    print(f"Extracted all files to: {extract_to}")
+
+def process_directory(directory):
+    extract_to = directory
+    with os.scandir(directory) as entries:
+        files = [entry.name for entry in entries if entry.is_file()]
+        for file in files:
+            if file.endswith(".zip"):
+                extract_to = os.path.join(directory, file[:-4])
+                extract_zip(os.path.join(directory, file), extract_to)
+                os.remove(os.path.join(directory, file))
+
+
+if __name__ == "__main__":
+    types = ["sitting-down", "standing-up", "walking", "running", "climbing-stairs"]
+    for type in types:
+        directory = f"data/{type}"
+        process_directory(directory)
+
+
